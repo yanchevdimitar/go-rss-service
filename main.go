b6955b36c76372d0ca7366a45750e7fe430e5bcb
Dimitar Yanchev <dimitar.yanchev@acronis.com>
2022-04-20 11:59:29 +0300

    Move connection and migration from main file

--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 
 	"github.com/joho/godotenv"
 	_ "github.com/joho/godotenv/autoload"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 
 	"github.com/yanchevdimitar/RSS-Reader-Service/app/database"
 	"github.com/yanchevdimitar/RSS-Reader-Service/app/services/queue"
@@ -22,33 +20,10 @@ func main() {
 		log.Fatalf("Error loading .env file")
 	}
 
-	db := DBInit()
-	Migrations(db)
+	db := database.DBInit()
+	database.Migrations(db)
 	go queue.NewDefaultConsumer(database.NewMySQLRSSRepository(db)).Process()
 	go queue.NewDefaultPublisher(database.NewMySQLRSSRepository(db)).Process()
 
 	runtime.Goexit()
-}
-
-func DBInit() *gorm.DB {
-	db, err := gorm.Open(mysql.New(mysql.Config{
-		DSN: "" + os.Getenv("MYSQL_USER") + ":" + os.Getenv("MYSQL_PASSWORD") + "@tcp(" + os.Getenv("MYSQL_HOST") + ":" + os.Getenv("MYSQL_PORT") + ")/" +
-			os.Getenv("MYSQL_DATABASE") + "?charset=utf8&parseTime=True&loc=Local",
-	}), &gorm.Config{})
-
-	if err != nil {
-		log.Fatalf("Error conecting DB")
-	}
-
-	return db
-}
-
-func Migrations(db *gorm.DB) {
-	checkTable := db.Migrator().HasTable("rss")
-	if !checkTable {
-		err := db.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(&database.RSS{})
-		if err != nil {
-			log.Fatalf("Error running Migrations")
-		}
-	}
 }
